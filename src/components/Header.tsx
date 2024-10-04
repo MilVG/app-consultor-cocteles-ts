5bdcf2ecea4e7b8c37fad75fc3e483c2fb2a443c
notlim_dev <elmotigre6@gmail.com>
2024-10-04 14:12:29 -0500

    0007: Implementación Link de ruta de Pagina
    Se utilizó el componente Link de react-router-dom para establecer las
    rutas de las paginas es como tal no ayuda a tener la navegacion entre
    paginas creadas.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
+
           <div>
             <img className="w-32" src="/logo.svg" alt="logotipo" />
           </div>
+
+          <nav className="flex gap-4">
+            <Link
+              to="/"
+              className="text-white uppercase font-bold hover:cursor-pointer hover:bg-orange-500 rounded-lg p-2 transition"
+            >Inicio</Link>
+            <Link
+              to="/favorites"
+              className="text-white uppercase font-bold hover:cursor-pointer hover:bg-orange-500 rounded-lg p-2 transition"
+            >Favoritos</Link>
+          </nav>
+
         </div>
       </div>
     </header>
