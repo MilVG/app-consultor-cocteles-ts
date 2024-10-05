a63345d72811821bfecb117dd22ac5997f94df66
notlim_dev <elmotigre6@gmail.com>
2024-10-05 15:07:55 -0500

    0022: Proceso de Mostrar Interfaz de datos-State
    1º => Se implmentó el tipado para state drink en singular inferido de
    schema de zod.
    2º => se creó el componente especifico para mostrar los datos
    (DrinkCard).
    3º => se incorporó el componente al indexPage y se pasaron las
    propiedades del satate.

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,30 @@
-
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+import Drinkcard from "../components/DrinkCard"
 
 export default function IndexPage() {
+
+  const drinks = useAppStore((state) => state.drinks)
+
+  const VerificarDatosVaciosDrinks = useMemo(() => drinks.drinks.length > 0, [drinks])
   return (
     <>
-      <h1>Inicio</h1>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+
+      {VerificarDatosVaciosDrinks ? (
+        <div className="grid grid-c md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {drinks.drinks.map(drink => (
+            <Drinkcard
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          No hay resultados aún, utiliza el formulario para buscar recetas
+        </p>
+      )}
     </>
   )
 }
