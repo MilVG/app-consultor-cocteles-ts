9c167f7fc098348fe60581cc65738e900b1dc454
notlim_dev <elmotigre6@gmail.com>
2024-10-05 13:09:19 -0500

    0018: Incorporar devtools zustand
    => Envolvemos dentro de función create de zustand todos los elementos,
    esto nos proporciona poder utilziar la extension redux-devtools en el
    navegador.

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipeSliceType } from "./recipeSlice";
+import { devtools } from "zustand/middleware";
 
-export const useAppStore = create<RecipeSliceType>((...a) => ({
-  ...createRecipesSlice(...a)
-}))
+export const useAppStore = create<RecipeSliceType>()(
+  devtools(
+    (...a) => ({
+      ...createRecipesSlice(...a)
+    })))
