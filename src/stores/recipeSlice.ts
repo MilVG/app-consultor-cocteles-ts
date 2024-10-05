004b289095219f601a8755e3c692ef4e5fda5363
notlim_dev <elmotigre6@gmail.com>
2024-10-05 01:03:13 -0500

    0012: Creación Context con zustand Store.
    1º => Se crearon los archivos de store, el primero useApp es quien
    manejará todos los stores mas especificos mejor dicho el main de los
    stores.
    2º => se definó la implementación que deben llevar cad auno de ellos
    como base.
    3º => se incorporó el tipado en cada uno de ellos.

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,12 @@
+import { StateCreator } from "zustand"
+
+type Category = {
+
+}
+
+export type RecipeSliceType = {
+  categories: Category[]
+}
+export const createRecipesSlice: StateCreator<RecipeSliceType> = () => ({
+  categories: []
+})
