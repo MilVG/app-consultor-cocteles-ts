751aad3b792206ff885f6cf7e20eeee261bd6ce7
notlim_dev <elmotigre6@gmail.com>
2024-10-06 13:55:59 -0500

    0032: Listar Datos Favoritos-Pagefavorites
    1º =>  se implementó la Interfaz para mostar los datos en este caso se
    reutilizó le DrinkCard que creamos anteriormente.

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,6 +1,31 @@
+import { useMemo } from "react"
+import Drinkcard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function FavoritesPage() {
+
+  const favorites = useAppStore((state) => state.favorites)
+  const comprobarDatosFavorites = useMemo(() => favorites.length > 0, [favorites])
+
   return (
-    <div>FavoritesPage</div>
+    <>
+      <h1 className="text-6xl font-extrabold">favorites</h1>
+      {comprobarDatosFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map(drink => (
+            <Drinkcard
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          Los favoritos se mostrarn aqui{' '}
+          <span className="text-orange-600"> Agrega Alguno de ellos</span>
+        </p>
+      )}
+    </>
   )
 }
 
