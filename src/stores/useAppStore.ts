d948d2c781aa23dea02b285a92cbd9e251b80bec
notlim_dev <elmotigre6@gmail.com>
2024-10-06 12:22:48 -0500

    0028: Creación de slice Context favorites
    Se creó y  Agrefó un subContext store para agregar items a favoritos.

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipeSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
-
-export const useAppStore = create<RecipeSliceType>()(
+import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
   devtools(
     (...a) => ({
-      ...createRecipesSlice(...a)
+      ...createRecipesSlice(...a),
+      ...createFavoritesSlice(...a),
     })))
