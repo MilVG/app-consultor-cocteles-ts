a69461dc8b3d7c6810c46648b811047c92b3ef22
notlim_dev <elmotigre6@gmail.com>
2024-10-06 14:47:48 -0500

    0035: Intercambio de datos entre Slice para las Notificaciones.
    1º => se Implementó los tipados en diferntes slice para poder llamar una
    función dentro de un slice , en este caso se incorporó la funcion de
    showNotication dentro del slice favorites.

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,6 +1,7 @@
 import { StateCreator } from "zustand";
 import { Recipe } from "../types";
 import { createRecipesSlice, RecipeSliceType } from "./recipeSlice";
+import { createNotificatiosSlice, NotificationSliceType } from "./notificationSlice";
 
 export type FavoritesSliceType = {
   favorites: Recipe[]
@@ -9,17 +10,19 @@ export type FavoritesSliceType = {
   loadFromStorage: () => void
 }
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipeSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipeSliceType & NotificationSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
   favorites: [],
   handleClickFavorite: (recipe) => {
     if (get().favoritesExists(recipe.idDrink)) {
       set((state) => ({
         favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
       }))
+      createNotificatiosSlice(set, get, api).showNotification({ text: 'Se eliminó correctamente de favoritos', error: false })
     } else {
       set((state) => ({
         favorites: [...state.favorites, recipe]
       }))
+      createNotificatiosSlice(set, get, api).showNotification({ text: 'Se agregó a favoritos', error: false })
     }
     createRecipesSlice(set, get, api).closeModal()
     localStorage.setItem('favorites', JSON.stringify(get().favorites))
--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -1,4 +1,5 @@
 import { StateCreator } from "zustand";
+import { FavoritesSliceType } from "./favoritesSlice";
 
 type Notification = {
   text: string
@@ -7,12 +8,35 @@ type Notification = {
 }
 export type NotificationSliceType = {
   notification: Notification
+  showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
+  hideNotification: () => void
 }
 
-export const createNotificatiosSlice: StateCreator<NotificationSliceType> = (set, get) => ({
+export const createNotificatiosSlice: StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = (set, get) => ({
   notification: {
     text: '',
     error: false,
     show: false
+  },
+  showNotification: (payload) => {
+    set({
+      notification: {
+        text: payload.text,
+        error: payload.error,
+        show: true
+      }
+    })
+    setTimeout(() => {
+      get().hideNotification()
+    }, 2000)
+  },
+  hideNotification: () => {
+    set({
+      notification: {
+        text: '',
+        error: false,
+        show: false
+      },
+    })
   }
 })
